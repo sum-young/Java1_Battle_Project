213d3d65fe03c0d5bc885db1374a4cfb18edb789
coderin <hyerinpark@ewha.ac.kr>
2025-06-06 15:19:56 +0900

    emotion에 String s에 저장

--- lastfile/src/emotion/Emotion.java
+++ lastfile/src/emotion/Emotion.java
@@ -22,32 +22,45 @@ public abstract class Emotion implements Attackable, Healable, Buffable, Debuffa
     
     public void attack(Emotion target) {
     	target.setCurr_hp(target.getCurr_hp() - this.attack_power);
-//		if (this.curr_hp / this.max_hp <= 0.3) {
+//		if ((double)this.curr_hp / this.max_hp <= 0.3) {
 //			this.weaponAttack(target);
+//			return;
 //		}
+		String s = this.name + "가 " + target.name + "를 공격합니다!"; 
+		if (text != null) text.updateText(s);
+		text.updateText(s);
     }
     
     public void weaponAttack(Emotion target){
 		target.setCurr_hp(target.getCurr_hp() - this.weapon.getPower());
-	}
+		String s = this.name + "가" + this.weapon + "으로" +target.name+"를 공격합니다.";
+		if (text != null) text.updateText(s);
+		// NullPointerException 때문에 if(text != null) 추가함
+
+    }
     
     public void heal() {
 		int random_num = (int)(Math.random() * 100 % 2); //2의 배수면 힐 성공, 아니면 실패
 		if(random_num == 0) {
+			// 힐 조건 수정해야할거같습니다
 			this.curr_hp += heal_power < max_hp ? heal_power : max_hp;
+			
+			String s = this.name +"가 힐을 성공했습니다.\n"+"hp가 " + heal_power + "만큼 추가되어 " + this.curr_hp + " 됐습니다!";
+			if (text != null) text.updateText(s);
 		}
 		else {
+			String s = this.name+"가 힐을 실패했습니다ㅠㅠ";
 			return;
 		}
 	}
     
     public void debuff() {
     	this.attack_power *= 0.97;
-    	System.out.println(this.name + "이 디버프를 받습니다.");
+    	System.out.println(this.name + "가 디버프를 받습니다.");
     }
     public void buff() {
     	this.attack_power *= 1.05;
-    	System.out.println(this.name + "이 버프를 받습니다.");
+    	System.out.println(this.name + "가 버프를 받습니다.");
     }
 
 	
